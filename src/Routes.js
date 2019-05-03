58323eb7f50f58cd05a345e5b5eae408805fdedb
Connor Tangney <ctangney@pandrdental.com>
2019-05-03 16:29:16 -0400

    using hashrouter for github pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-quotes */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './routes/Home/Home';
 
 export default function Routes() {
